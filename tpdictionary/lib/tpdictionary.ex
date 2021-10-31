0d6913948738283c3ff9cb488ca55418cb5d846e
TomasBruno1 <tomas.bruno@ing.austral.edu.ar>
2021-10-31 11:35:54 -0300

    tpdictionary optimizado

--- tpdictionary/lib/tpdictionary.ex
+++ tpdictionary/lib/tpdictionary.ex
@@ -1,9 +1,9 @@
 defmodule Translator do
 
-  defmodule Translator.State do
-    defstruct document_count: 0, word_count: 0, word_frequency: %{}
+  defmodule State do
+    defstruct dictionary: , document_count: 0, word_count: 0, word_frequency: %{}
       def new_state(document_count, word_count, word_frequency) do
-        %Translator.State{
+        %State{
           document_count: document_count,
           word_count: word_count,
           word_frequency: word_frequency
@@ -11,47 +11,39 @@ defmodule Translator do
       end
   end
 
-  def start() do
-    spawn(&loop/0)
+  def start(dict) do
+    spawn(fn -> loop(dict) end)
   end
 
   def loop() do
-    loop(%Translator.State{})
+    loop(%State{})
   end
 
-  def translate(document, dictionary) do
-    document
-    |> String.trim()
-    |> String.split(" ")
-    |> Enum.map(fn word ->  Map.get(dictionary, word) end)
-    |> Enum.join(" ")
-  end
-
-  def update_state(translation, state) do
-    word_count =
-      translation
-      |> String.split(" ")
-      |> Kernel.length()
-
-    updated_word_count = word_count + state.word_count
-    updated_document_count = if (word_count > 1) do state.document_count + 1 else state.document_count end
-
-    frequency =
-      translation
-      |> String.split(" ")
-      |> Enum.frequencies()
-
-    updated_word_frequency =  Map.merge(state.word_frequency, frequency, fn _k, v1, v2 -> v1 + v2 end)
-
-    %{state | document_count: updated_document_count, word_count: updated_word_count, word_frequency: updated_word_frequency}
-  end
-
-  def loop(state) do
+  def loop(dict, state) do
     receive do
       {:translate, from, document, dictionary} ->
-        translation = translate(document, dictionary)
+        translation = document
+                      |> String.trim()
+                      |> String.split(" ")
+                      |> Enum.map(fn word ->  Map.get(dictionary, word) end)
+                      |> Enum.join(" ")
         send(from, {:translation, translation})
-        loop(update_state(translation, state))
+        word_count =
+              translation
+              |> String.split(" ")
+              |> Kernel.length()
+
+        updated_word_count = word_count + state.word_count
+        updated_document_count = if (word_count > 1) do state.document_count + 1 else state.document_count end
+
+        frequency =
+            translation
+            |> String.split(" ")
+            |> Enum.frequencies()
+
+        updated_word_frequency =  Map.merge(state.word_frequency, frequency, fn _k, v1, v2 -> v1 + v2 end)
+
+        loop(%{state | document_count: updated_document_count, word_count: updated_word_count, word_frequency: updated_word_frequency})
 
       {:stats, from} ->
         send(from,{:state, state})
